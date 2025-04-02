3fe6a1290f23bbd5beb9bc4d75e69b08cc73204a
estudosRyan <lucca.ryan.gomes@gmail.com>
2025-04-02 14:53:47 -0300

    Arrumando para subir no render

--- server.js
+++ server.js
@@ -57,6 +57,8 @@ io.on("connection", (socket) => {
     });
 });
 
-server.listen(3000, () => {
-    console.log("Servidor rodando em http://localhost:3000");
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`);
 });
+
